0737534a85b0aa67c86fadf1a17d12258092afee
Deva93 <devavijay@rocketmail.com>
2023-02-27 07:54:40 +0000

    saving changes to test poll scm

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("JENKINS IS AWESOME!");
+        System.out.println("Updating to test scm poll!");
     }
 
     public String getMessage() {
