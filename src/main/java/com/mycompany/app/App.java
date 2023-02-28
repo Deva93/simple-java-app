4efc5a8ce1d6545fd9113d03f20c67573bf632c2
Deva93 <devavijay@rocketmail.com>
2023-02-28 05:37:13 +0000

    testing poll scm in pipeline

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Trying github webhook!");
+        System.out.println("Testing poll SCM!");
     }
 
     public String getMessage() {
